274636c07d18848da0481711d83797c2594284ec
Dmitry Eronin <dimaromanov06@icloud.com>
2025-01-19 00:16:19 +0300

    complete task 2.1.4

---
+++ src/main/java/koschei/config/AppConfig.java
@@ -0,0 +1,54 @@
+package koschei.config;
+
+import koschei.models.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "koschei")
+public class AppConfig {
+
+    @Bean
+    public Ocean1 getOcean(@Qualifier("getIsland") Island2 island) {
+        return new Ocean1(island);
+    }
+
+    @Bean
+    public static Island2 getIsland(@Qualifier("getWood") Wood3 wood) {
+        return new Island2(wood);
+    }
+
+    @Bean
+    public static Wood3 getWood(@Qualifier("getRabbit") Rabbit4 rabbit) {
+        return new Wood3(rabbit);
+    }
+
+    @Bean
+    public static Rabbit4 getRabbit(@Qualifier("getDuck") Duck5 duck) {
+        return new Rabbit4(duck);
+    }
+
+    @Bean
+    public static Duck5 getDuck(@Qualifier("getEgg") Egg6 egg) {
+        return new Duck5(egg);
+    }
+
+    @Bean
+    public static Egg6 getEgg(@Qualifier("getNeedle") Needle7 needle) {
+        return new Egg6(needle);
+    }
+
+    @Bean
+    public static Needle7 getNeedle(@Qualifier("getDeth8") Deth8 deth) {
+        return new Needle7(deth);
+    }
+
+    @Bean
+    public static Deth8 getDeth8() {
+        return new Deth8();
+    }
+
+}
